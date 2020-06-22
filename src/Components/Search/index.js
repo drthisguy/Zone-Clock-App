a6866009fc9fb4af04ae09eb02023260a154a6cc
drthisguy <page.c.tyler@gmail.com>
2020-06-22 16:35:16 -0400

    search fields and button added with some sytling/spacing.

---
+++ src/Components/Search/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const SearchField = props => <input className="form-control" {...props} />
+
+export const Button = props => <button className="btn btn-primary btn-lg btn-block" {...props} >{props.children}</button>
+
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import logo from '../../logo.svg';
-import { Container, Row } from '../../components/Grid';
+import { Container, Row, Col } from '../../components/Grid';
 import ClockMount from '../../components/ClockMount';
+import { SearchField, Button } from '../../components/Search';
 
 export default function Main() {
     return (
         <Container >
         <ClockMount />
         <Row >
-          
+          <Col size="md-3" >
+            <form class="form-group my-4">
+                <SearchField placeholder="Search a City..." />
+                <SearchField placeholder="State or Country.." />
+                <Button>Search</Button>
+            </form>
+          </Col>
         </Row>
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
