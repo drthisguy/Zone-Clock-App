1b669849394b41995a0d2abaace44d2d35661588
My Lord <page.c.tyler@gmail.com>
2020-07-03 20:36:35 -0400

    analog clock background set in place. sec hand is ticking albiet incorrectly.

--- client/src/components/AnalogClock/index.js
+++ client/src/components/AnalogClock/index.js
@@ -10,14 +10,16 @@ export function AnalogClock({ offset }) {
         }, [offset])
 
 
-    const formatTime = time => time < 10 ? `0${time}` : time,
+    const formatTime = time => {
+        console.log(time)
+        return time < 10 ? `0${time}` : time},
 
     localClock = () => {
         const date = new Date();
-        date.setHours(date.getHours + offset);
-        let hourHand = formatTime(date.getHours),
-         minuteHand = formatTime(date.getMinutes),
-         secondHand = formatTime(date.getSeconds);
+        date.setHours(date.getHours() + offset);
+        let hourHand = formatTime(date.getHours()),
+         minuteHand = formatTime(date.getMinutes()),
+         secondHand = formatTime(date.getSeconds());
          setHands({ hourHand, minuteHand, secondHand })
     }
 
@@ -26,7 +28,7 @@ export function AnalogClock({ offset }) {
     return (
         <div style={mount} >
             <div style={analog}>
-                <div style={{...clockHands, backgroundImage: `url(${require("../../assets/img/second-hand.png")}`, transform: `rotate(${secondHand * 6}deg)`}} />
+                <img src={require('../../assets/img/second-hand.png')} style={{...clockHands, transform: `rotate(${secondHand * 6}deg)`}} />
                 <div style={{...clockHands, transform: `rotate(${minuteHand * 6}deg)`}} />
                 <div style={{...clockHands, transform: `rotate(${hourHand * 30}deg)`}} />             
             </div>
@@ -35,7 +37,7 @@ export function AnalogClock({ offset }) {
 }
 
 const mount = {
-    width: '250px',
+    // width: '400px',  
     textAlign: 'center',
     display: 'flex',
     flexDirection: 'column',
@@ -43,10 +45,15 @@ const mount = {
 },
 analog = {
     width: '250px',
+    height: '250px',
     position: 'relative',
     backgroundImage: `url(${require("../../assets/img/clock-ABS.png")}`,
+    backgroundSize: '250px',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
 },
 clockHands = {
+    width: '10px',
     position: 'absolute',
     transformOrigin: 'bottom left'
 }
