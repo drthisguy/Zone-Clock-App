a1382a11b8e0b82c6570ec7f6ea08ca214398b73
My Lord <page.c.tyler@gmail.com>
2020-07-07 21:02:35 -0400

    programmed the datepicker component properly to display the dst start date.

--- client/src/components/DatePicker/index.js
+++ client/src/components/DatePicker/index.js
@@ -5,18 +5,18 @@ import 'react-day-picker/lib/style.css';
 export function DatePicker({ dst }) {
 
     const modifiers = {
-        highlighted: new Date('2020, 7, 19'),
+        highlighted: dst,
       };
 
     return (
         <div>
             <style>{birthdayStyle}</style>
-            <DayPicker modifiers={modifiers} month={new Date(2020, 7)} />
+            <DayPicker modifiers={modifiers} month={new Date(dst.getFullYear(), dst.getMonth())} />
         </div>
     )
 }
 
 const birthdayStyle = `.DayPicker-Day--highlighted {
-    background-color: orange;
+    background-color: #fcaf17;
     color: white;
   }`;
