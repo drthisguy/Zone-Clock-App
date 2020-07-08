8e6923ebb88161e8fffd1470951596a5654f7e8e
drthisguy <page.c.tyler@gmail.com>
2020-07-08 10:39:19 -0400

    setup the logic not return datepickers and render other information when daylight savings is 'none'.

--- client/src/components/DaylightSavings/DaylightSavings.js
+++ client/src/components/DaylightSavings/DaylightSavings.js
@@ -5,14 +5,18 @@ import { DatePicker } from '../DatePicker'
 export function DaylightSavings({ dstStart, dstEnd }) {
 
 
-    return (
-        <Row >
-            <Col size='md-6'>
-                <DatePicker dst={dstStart} right={{float: 'right'}} />
-            </Col>
-            <Col size='md-6'>
-                <DatePicker dst={dstEnd} right={{}} />
-            </Col>
-      </Row>
-    )
+    if (dstEnd === 'none') {
+        return <h2>No Daylight Savings</h2>  
+    } else {
+        return (
+            <Row >
+                <Col size='md-6'>
+                    <DatePicker dst={dstStart} right={{float: 'right'}} />
+                </Col>
+                <Col size='md-6'>
+                    <DatePicker dst={dstEnd} right={{}} />
+                </Col>
+        </Row>
+        )
+    }
 }
