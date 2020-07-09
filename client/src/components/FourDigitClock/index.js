0b7378578990a73b6268dbb8a3dd17da4f8290bc
drthisguy <page.c.tyler@gmail.com>
2020-07-09 15:05:42 -0400

    set up component for 4 digit clocks to be used with the history list.  rendering local time okay.

---
+++ client/src/components/FourDigitClock/index.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { getLocalTime } from '../../utils/Helpers';
+
+export function FourDigitClock({ offset }) {
+
+    const [ timeString, setTimeString ] = useState('12:00:00 AM');
+
+    useEffect(() => {
+        const timer = setInterval(localClock, 1000)  
+            return () => clearInterval(timer)
+    }, [offset])
+
+
+    const localClock = () => {
+
+        let { localHours, localMinutes } = getLocalTime(offset);
+
+            localMinutes = (localMinutes < 10 ? '0' : '') + localMinutes;
+
+        const timeOfDay = localHours < 12  ? 'am' : 'pm';
+            localHours = localHours > 12  ? localHours - 12 : localHours;
+            localHours = localHours == 0  ? 12 : localHours;
+
+        const localTimeString = `${localHours}:${localMinutes}:${timeOfDay}`;
+        setTimeString(localTimeString);   
+    }  
+
+    return (
+        <div>
+            {timeString}
+        </div>
+    )
+}
--- client/src/components/HistoryList/index.js
+++ client/src/components/HistoryList/index.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from 'react'
+import { FourDigitClock } from '../FourDigitClock'
 
 export  function HistoryList({ data }) {
 
     const listCities = cities => {
         return (
             cities.map( city => 
-            <li className="list-group-item d-flex justify-content-between align-items-center">
-            {city.names.shortName}
-            <span class=''>12:00pm</span>
-        </li>
-        )
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                    {city.names.shortName}
+                    <span className=''><FourDigitClock offset={city.rawOffset} /></span>
+                </li>
+            )
         )
     }
 
@@ -17,7 +18,9 @@ export  function HistoryList({ data }) {
     return (
         <div>
             <ul class="list-group">
-               {listCities(data)}
+                <Fragment>
+                     {listCities(data)}
+                </Fragment>
             </ul>
             
         </div>
