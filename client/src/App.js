7e06f25d1784f551338d2317974348a42ed01dd2
drthisguy <page.c.tyler@gmail.com>
2020-07-10 13:57:47 -0400

    Heroku Deployment.  completed About.js and set up its rout up in App.js. Updated discription in README.md.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Main from './pages/Main'
+import About from './pages/About'
 import './App.css';
 
 
 function App() {
   return (
     <Router >
-    <Navbar />
-    <Main />
+      <Navbar />
+      <Route exact path="/"><Main /></Route>
+      <Route exact path="/about" ><About /></Route>
     </Router>
   );
 }
